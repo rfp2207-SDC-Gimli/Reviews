f21c54aa8f7c338b8202f0f3d40eb87b83c160f4
Gracie Fogarty <graciealysse@gmail.com>
2022-09-21 14:39:06 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.get('/loaderio-81f1611dd826ba094dc2079fe42ee64a', (req, res) => 
   res.send('loaderio-81f1611dd826ba094dc2079fe42ee64a')
 );
-loaderio-81f1611dd826ba094dc2079fe42ee64a;
+
 //routes for the '/reviews' endpoint
 app.get('/reviews', (req, res) => {
   const params = req.query;
